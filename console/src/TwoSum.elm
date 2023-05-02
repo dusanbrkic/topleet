74bfbcbef3b1bb59556acc1339702d6ea61d699a
Dusan Brkic <dusanbrk@gmail.com>
2023-05-03 00:51:05 +0200

    twoSum in elm

---
+++ console/src/TwoSum.elm
@@ -0,0 +1,30 @@
+module TwoSum exposing (..)
+
+import Dict exposing (Dict)
+
+
+twoSum : List Int -> Int -> Maybe (List Int)
+twoSum nums target =
+    nums
+        |> List.indexedMap (\i num -> ( i, num ))
+        |> List.foldr
+            (\( i, num ) ( complements, acc ) ->
+                case ( Dict.get (target - num) complements, acc ) of
+                    ( _, Just _ ) ->
+                        ( complements, acc )
+
+                    ( Just complement, _ ) ->
+                        ( complements, Just ( i, complement ) )
+
+                    ( Nothing, _ ) ->
+                        ( Dict.insert num i complements, Nothing )
+            )
+            ( Dict.empty, Nothing )
+        |> (\( _, acc ) ->
+                case acc of
+                    Just ( first, second ) ->
+                        Just [ first, second ]
+
+                    Nothing ->
+                        Nothing
+           )
---
+++ console/tests/TwoSumTests.elm
@@ -0,0 +1,23 @@
+module TwoSumTests exposing (..)
+
+import Expect exposing (..)
+import Test exposing (..)
+import TwoSum exposing (twoSum)
+
+
+suite : Test
+suite =
+    describe "Default Tests"
+        [ test "test1" <|
+            \_ ->
+                twoSum [ 2, 7, 11, 15 ] 9
+                    |> Expect.equal [ 0, 1 ]
+        , test "test2" <|
+            \_ ->
+                twoSum [ 3, 2, 4 ] 6
+                    |> Expect.equal [ 1, 2 ]
+        , test "test3" <|
+            \_ ->
+                twoSum [ 3, 3 ] 6
+                    |> Expect.equal [ 0, 1 ]
+        ]
